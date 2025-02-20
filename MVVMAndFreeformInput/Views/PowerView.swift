4cf427d827160e076f594a6ec24babcb4e403ee8
Tiffany Zhang <tzhang@lcs.on.ca>
2025-02-20 11:11:04 -0500

    added () for negative base

--- MVVMAndFreeformInput/Views/PowerView.swift
+++ MVVMAndFreeformInput/Views/PowerView.swift
@@ -24,10 +24,13 @@ struct PowerView: View {
                 // we write a power on paper in math class
                 HStack(alignment: .center) {
                     HStack(alignment: .top) {
-                        
-                        Text("\(power.base.formatted())")
-                            .font(.system(size: 96))
-                        
+                        if power.base < 0 {
+                            Text("(\(power.base.formatted()))")
+                                .font(.system(size: 96))
+                        }else {
+                            Text("\(power.base.formatted())")
+                                .font(.system(size: 96))
+                        }
                         Text("\(power.exponent)")
                             .font(.system(size: 44))
                     }
