60567e149ce37dcb74f27774cdf53059775856d2
Tiffany Zhang <tzhang@lcs.on.ca>
2025-02-20 09:40:34 -0500

    Initial Commit

---
+++ MVVMAndFreeformInput/MVVMAndFreeformInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMAndFreeformInputApp.swift
+//  MVVMAndFreeformInput
+//
+//  Created by xinyu zhang on 2025-02-20.
+//
+
+import SwiftUI
+
+@main
+struct MVVMAndFreeformInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
