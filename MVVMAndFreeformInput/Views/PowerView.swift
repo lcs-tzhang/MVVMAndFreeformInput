9b2a11a3f80775afef150bd0dba656b8aab45c8c
Tiffany Zhang <tzhang@lcs.on.ca>
2025-02-20 10:02:20 -0500

    Refined the view so it presents the power in a way that better matches symbolic mathematics standards; showing a nicer user interface overall.

--- MVVMAndFreeformInput/Views/PowerView.swift
+++ MVVMAndFreeformInput/Views/PowerView.swift
@@ -1,38 +1,76 @@
-//
-//  ContentView.swift
-//  MVVMAndFreeformInput
-//
-//  Created by xinyu zhang on 2025-02-20.
-//
-
 import SwiftUI
-
+ 
 struct PowerView: View {
+    
     // MARK: Stored properties
     
     // Holds the view model, to track current state of
     // data within the app
     @State var viewModel = PowerViewModel()
-    
+ 
     // MARK: Computed properties
     var body: some View {
         VStack {
             
-            // INPUT
-            TextField("Base", text: $viewModel.providedBase)
-            TextField("Exponent", text: $viewModel.providedExponent)
+            // Extra space at top
+            Spacer()
             
             // OUTPUT
+            // When the power can be unwrapped, show the result
             if let power = viewModel.power {
-                Text("Result is: \(power.result)")
+                
+                // Show the provided base, exponent, and result
+                // in an arrangement that looks the same as how
+                // we write a power on paper in math class
+                HStack(alignment: .center) {
+                    HStack(alignment: .top) {
+                        
+                        Text("\(power.base.formatted())")
+                            .font(.system(size: 96))
+                        
+                        Text("\(power.exponent)")
+                            .font(.system(size: 44))
+                    }
+                    HStack {
+ 
+                        Text("=")
+                            .font(.system(size: 96))
+ 
+                        Text("\(power.result.formatted())")
+                            .font(.system(size: 96))
+                    }
+                }
+                .lineLimit(1)
+                .minimumScaleFactor(0.5)
+                .frame(height: 300)
+ 
+            } else {
+                
+                // Show a message indicating that we are
+                // awaiting reasonable input
+                ContentUnavailableView(
+                    "Unable to evaluate power",
+                    systemImage: "gear.badge.questionmark",
+                    description: Text(viewModel.recoverySuggestion)
+                )
+                .frame(height: 300)
             }
-            Text(viewModel.recoverySuggestion)
             
+            // INPUT
+            TextField("Base", text: $viewModel.providedBase)
+                .textFieldStyle(.roundedBorder)
+            
+            TextField("Exponent", text: $viewModel.providedExponent)
+                .textFieldStyle(.roundedBorder)
+ 
+            // Extra space at bottom
+            Spacer()
         }
         .padding()
     }
+ 
 }
-
+ 
 #Preview {
     PowerView()
 }
