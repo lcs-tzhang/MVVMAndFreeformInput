3d8f2d33c7821ea33fdd8e80f51006770027a81d
Tiffany Zhang <tzhang@lcs.on.ca>
2025-02-22 10:23:37 -0500

    Added a button to the view to invoke the function from the view model that saves an evaluated power to our history of previously evaluated powers.

--- MVVMAndFreeformInput/Views/PowerView.swift
+++ MVVMAndFreeformInput/Views/PowerView.swift
@@ -53,6 +53,18 @@ struct PowerView: View {
                 .minimumScaleFactor(0.5)
                 .frame(height: 300)
                 
+                // Add a button so that the result can be saved
+                
+                Button {
+                    viewModel.saveResult()
+                    // DEBUG: Show how many items are in the resultHistory array
+                    print("There are \(viewModel.resultHistory.count) elements in the resultHistory array.")
+                } label: {
+                    Text("Save")
+                }
+                .buttonStyle(.borderedProminent)
+                .padding(.bottom)
+                
             } else {
                 
                 // Show a message indicating that we are
